14e296123335fcf464c3ee5a535c233949234f75
Umar <ugairbekov1911@gmail.com>
2025-10-28 16:17:09 +0400

    feat: do smth

--- js/script.js
+++ js/script.js
@@ -18,3 +18,38 @@ const a = prompt("Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
+
+if (1) {
+  console.log("ok!");
+} else {
+  console.log("Error");
+}
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('много');
+// } else {
+//     console.log('ok');
+// }
+
+// num === 50 ? console.log("ok") : console.log("Error");
+
+// const num = 51;
+
+// switch (num) {
+//     case 49:
+//         console.log('not');
+//         break;
+//     case 100:
+//         console.log('not');
+//         break;
+//     case 50:
+//         console.log('yes');
+//         break;
+//     default:
+//         console.log('не в этот раз');
+//         break;
+// }
