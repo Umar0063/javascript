946953278cfdeaded36b0d92dacf807f904feab5
Umar <ugairbekov1911@gmail.com>
2025-10-28 15:17:33 +0400

    first application

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const numberOfFilm = +prompt("Сколько фильмов вы уже смотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilm,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько его оцените?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько его оцените?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
